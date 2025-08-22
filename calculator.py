6f6efb3886edce924fc671d9ddbb086081c8ccf6
Niikiim <nikolas_marx@hotmail.com>
2025-08-22 18:00:43 -0300

    Atividade Desconto

---
+++ calculator.py
@@ -0,0 +1,19 @@
+#Ddesconto não pode ser maior que 100%
+#Desconto não pode ser menor que 0%
+#Desconto não pode ser maior que 100 reais
+#Qual o valor do desconto maximo ?
+
+
+
+
+price = float(input("Digite o preço: \n"))
+desc = float(input("Qual o desconto a ser aplicado:\n"))
+
+if desc >= 10 or desc <0:
+  
+    desc = (desc/100)*price
+    result = price - desc
+    print(f"O valor do desconto é {result}")
+    
+else:
+    print("O desconto não pode ser maior que 100%")
