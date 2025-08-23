04f3ded2e48c5979176f01071555e4bb1d3856ce
Niikiim <nikolas_marx@hotmail.com>
2025-08-23 13:29:49 -0300

    janela da frontend abrindo

--- font.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-import calculator
-
-window = tk.Tk
-window.title = "Calculadora de desconto"
-window.geometry("1000x1000")
--- front.py
+++ front.py
@@ -1,13 +1,12 @@
 import tkinter as tk
-import calculator
 
-window = tk.Tk
+window = tk.Tk()
 window.title = "Calculadora de desconto"
-window.geometry("1000x1000")
+window.geometry("500x500")
 
-frame_num = tk.frame(window)
+frame_num = tk.Frame(window)
 frame_num.pack(pady=15)
-tk.label(frame_num, text="Digite o valor do produto:").pack(side=tk.CENTER,padx=(0,16))
+tk.Label(frame_num, text="Digite o valor do produto:").pack(side=tk.LEFT,padx=(0,16))
 
 
-
+window.mainloop()
