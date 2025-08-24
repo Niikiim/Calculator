625385d612f8df8ddc4e614a495c0bcc37a78196
Niikiim <nikolas_marx@hotmail.com>
2025-08-24 11:46:02 -0300

    Tentando fazer a calculadora funcionar

--- calculator.py
+++ calculator.py
@@ -1,21 +1,39 @@
-#Ddesconto não pode ser maior que 100%
+#Ddesconto não pode ser maior que 10%
 #Desconto não pode ser menor que 0%
 #Desconto não pode ser maior que 100 reais
 #Qual o valor do desconto maximo ?
 
-price = float(input("Digite o preço: \n"))
-desc = float(input("Qual o desconto a ser aplicado:\n"))
-
-
 def desconto (desc,price):
-    for sair != "sair":
+
+    try:
+        price = float(price)
+        desc = float(desc)
         
+    except(ValueError,TypeError):
+        return "Erro!\n Insira os valores numéricos válidos!"
 
-    if desc >= 10 or desc <0:
+    desc = (desc/100)
+    desc_amount = desc * price
+    result = price - desc
+        
+    if  0 <= desc <= 10 and desc_amount <= 100:
   
-        desc = (desc/100)*price
-        result = price - desc
-        print(f"O valor do desconto é {result}")
+        return(f"O valor do desconto é {result}")
 
-    else:
-        print("O desconto não pode ser maior que 100%")
+    elif desc > 10:
+
+        return("O desconto não pode ser maior que 10%")
+    
+    elif desc < 0: 
+         
+        return("O desconto não pode ser menor que 0!")
+    
+    elif result > 100:
+        
+        desc_max = (100/price)*100
+
+        return("O desconto não pode ser maior que 100 reais!\n" f"O desconto máximo será de {desc_max}")
+
+
+    
+
--- front.py
+++ front.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from calculator import desconto
 
 window = tk.Tk()
 window.title = "Calculadora de desconto"
@@ -7,6 +8,13 @@ window.geometry("500x500")
 frame_num = tk.Frame(window)
 frame_num.pack(pady=15)
 tk.Label(frame_num, text="Digite o valor do produto:").pack(side=tk.LEFT,padx=(0,16))
+entry_num = tk.Entry(frame_num)
+entry_num.pack(side=tk.LEFT, fill=tk.X, expand=True)
 
+
+frame_calc = tk.Frame(window)
+frame_calc.pack(pady=15)
+button_calc = tk.Button(frame_calc, text="Calcular desoconto", command=lambda:desconto)
+button_calc.pack(side=tk.LEFT,padx=10)
 
 window.mainloop()
