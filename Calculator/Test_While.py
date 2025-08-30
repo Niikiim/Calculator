b20328f67314c5a34a0c2920b23f05ece05ac820
Niikiim <nikolas_marx@hotmail.com>
2025-08-29 23:35:10 -0300

    Teste While funcionando

--- Calculator/Test_While.py
+++ Calculator/Test_While.py
@@ -2,25 +2,26 @@
 
 tries = 0
 
-try:
+while tries < 3:
 
     try:
         
         x = int(input("Digite o valor inicial:"))
         max = int(input("Digite o máximo:"))
 
+        
         while x < max+1:
             print(x)
             x += 1
-    
-    
+
+        break
+        
     except ValueError:
 
         print("Digite um valor válido:")
+        tries += 1
+    
 
-except tries !=3:
-
-    tries += tries
 
 
     
